a7f019148784c5f5fd684564219fec3ed8e68c75
Quentin Meulepas <quentin.meulepas@adn.be>
2020-04-07 10:01:39 +0200

    Add some missing Object Identifiers

--- src/common.ts
+++ src/common.ts
@@ -214,15 +214,137 @@ initOID('1.2.840.113549.3.7', '3desCBC')
 initOID('2.16.840.1.101.3.4.1.2', 'aesCBC128')
 initOID('2.16.840.1.101.3.4.1.42', 'aesCBC256')
 
-// certificate issuer/subject OIDs
-initOID('2.5.4.3', 'commonName')
-initOID('2.5.4.5', 'serialName')
-initOID('2.5.4.6', 'countryName')
-initOID('2.5.4.7', 'localityName')
-initOID('2.5.4.8', 'stateOrProvinceName')
-initOID('2.5.4.10', 'organizationName')
-initOID('2.5.4.11', 'organizationalUnitName')
-initOID('2.5.4.15', 'businessCategory')
+initOID('2.5.4.0', 'objectClass') // RFC 4512
+initOID('2.5.4.1', 'aliasedObjectName') // RFC 4512
+initOID('2.5.4.2', 'knowledgeInformation') // RFC 2256
+initOID('2.5.4.3', 'commonName') // RFC 4519
+initOID('2.5.4.4', 'surname') // RFC 4519
+initOID('2.5.4.5', 'serialName') // RFC 4519
+initOID('2.5.4.6', 'countryName') // RFC 4519
+initOID('2.5.4.7', 'localityName') // RFC 4519
+initOID('2.5.4.8', 'stateOrProvinceName') // RFC 4519
+initOID('2.5.4.9', 'street') // RFC 4519
+initOID('2.5.4.10', 'organizationName') // RFC 4519
+initOID('2.5.4.11', 'organizationalUnitName') // RFC 4519
+initOID('2.5.4.12', 'title') // RFC 4519
+initOID('2.5.4.13', 'description') // RFC 4519
+initOID('2.5.4.14', 'searchGuide') // RFC 4519
+initOID('2.5.4.15', 'businessCategory') // RFC 4519
+initOID('2.5.4.16', 'postalAddress') // RFC 4519
+initOID('2.5.4.17', 'postalCode') // RFC 4519
+initOID('2.5.4.18', 'postOfficeBox') // RFC 4519
+initOID('2.5.4.19', 'physicalDeliveryOfficeName') // RFC 4519
+initOID('2.5.4.20', 'telephoneNumber') // RFC 4519
+initOID('2.5.4.21', 'telexNumber') // RFC 4519
+initOID('2.5.4.22', 'teletexTerminalIdentifier') // RFC 4519
+initOID('2.5.4.23', 'facsimileTelephoneNumber') // RFC 4519
+initOID('2.5.4.24', 'x121Address') // RFC 4519
+initOID('2.5.4.25', 'internationaliSDNNumber') // RFC 4519
+initOID('2.5.4.26', 'registeredAddress') // RFC 4519
+initOID('2.5.4.27', 'destinationIndicator') // RFC 4519
+initOID('2.5.4.28', 'preferredDeliveryMethod') // RFC 4519
+initOID('2.5.4.29', 'presentationAddress') // RFC 2256
+initOID('2.5.4.30', 'supportedApplicationContext') // RFC 2256
+initOID('2.5.4.31', 'member') // RFC 4519
+initOID('2.5.4.32', 'owner') // RFC 4519
+initOID('2.5.4.33', 'roleOccupant') // RFC 4519
+initOID('2.5.4.34', 'seeAlso') // RFC 4519
+initOID('2.5.4.35', 'userPassword') // RFC 4519
+initOID('2.5.4.36', 'userCertificate') // RFC 4523
+initOID('2.5.4.37', 'cACertificate') // RFC 4523
+initOID('2.5.4.38', 'authorityRevocationList') // RFC 4523
+initOID('2.5.4.39', 'certificateRevocationList') // RFC 4523
+initOID('2.5.4.40', 'crossCertificatePair') // RFC 4523
+initOID('2.5.4.41', 'name') // RFC 4519
+initOID('2.5.4.42', 'givenName') // RFC 4519
+initOID('2.5.4.43', 'initials') // RFC 4519
+initOID('2.5.4.44', 'generationQualifier') // RFC 4519
+initOID('2.5.4.45', 'x500UniqueIdentifier') // RFC 4519
+initOID('2.5.4.46', 'dnQualifier') // RFC 4519
+initOID('2.5.4.47', 'enhancedSearchGuide') // RFC 4519
+initOID('2.5.4.48', 'protocolInformation') // RFC 2256
+initOID('2.5.4.49', 'distinguishedName') // RFC 4519
+initOID('2.5.4.50', 'uniqueMember') // RFC 4519
+initOID('2.5.4.51', 'houseIdentifier') // RFC 4519
+initOID('2.5.4.52', 'supportedAlgorithms') // RFC 4523
+initOID('2.5.4.53', 'deltaRevocationList') // RFC 4523
+initOID('2.5.4.54', 'dmdName') // RFC 2256
+initOID('2.5.4.65', 'pseudonym') // RFC 2985
+
+initOID('2.5.6.0', 'top') // RFC 4512
+initOID('2.5.6.1', 'alias') // RFC 4512
+initOID('2.5.6.2', 'country') // RFC 4519
+initOID('2.5.6.3', 'locality') // RFC 4519
+initOID('2.5.6.4', 'organization') // RFC 4519
+initOID('2.5.6.5', 'organizationalUnit') // RFC 4519
+initOID('2.5.6.6', 'person') // RFC 4519
+initOID('2.5.6.7', 'organizationalPerson') // RFC 4519
+initOID('2.5.6.8', 'organizationalRole') // RFC 4519
+initOID('2.5.6.9', 'groupOfNames') // RFC 4519
+initOID('2.5.6.10', 'residentialPerson') // RFC 4519
+initOID('2.5.6.11', 'applicationProcess') // RFC 4519
+initOID('2.5.6.12', 'applicationEntity') // RFC 4519
+initOID('2.5.6.13', 'dSA') // RFC 4519
+initOID('2.5.6.14', 'device') // RFC 4519
+initOID('2.5.6.15', 'strongAuthenticationUser') // RFC 4519
+initOID('2.5.6.16', 'certificationAuthority') // RFC 4519
+initOID('2.5.6.16.2', 'certificationAuthority-V2') // RFC 4519
+initOID('2.5.6.17', 'groupOfUniqueNames') // RFC 4519
+initOID('2.5.6.18', 'userSecurityInformation') // RFC 4519
+initOID('2.5.6.19', 'cRLDistributionPoint') // RFC 4519
+initOID('2.5.6.20', 'dmd') // RFC 4519
+initOID('2.5.6.21', 'pkiUser') // RFC 4519
+initOID('2.5.6.22', 'pkiCA') // RFC 4519
+initOID('2.5.6.23', 'deltaCRL') // RFC 4519
+
+initOID('2.5.13.0', 'objectIdentifierMatch')
+initOID('2.5.13.1', 'distinguishedNameMatch')
+initOID('2.5.13.2', 'caseIgnoreMatch')
+initOID('2.5.13.3', 'caseIgnoreOrderingMatch')
+initOID('2.5.13.4', 'caseIgnoreSubstringsMatch')
+initOID('2.5.13.5', 'caseExactMatch')
+initOID('2.5.13.6', 'caseExactOrderingMatch')
+initOID('2.5.13.7', 'caseExactSubstringsMatch')
+initOID('2.5.13.8', 'numericStringMatch')
+initOID('2.5.13.9', 'numericStringOrderingMatch')
+initOID('2.5.13.10', 'numericStringSubstringsMatch')
+initOID('2.5.13.11', 'caseIgnoreListMatch')
+initOID('2.5.13.12', 'caseIgnoreListSubstringsMatch')
+initOID('2.5.13.13', 'booleanMatch')
+initOID('2.5.13.14', 'integerMatch')
+initOID('2.5.13.15', 'integerOrderingMatch')
+initOID('2.5.13.16', 'bitStringMatch')
+initOID('2.5.13.17', 'octetStringMatch')
+initOID('2.5.13.18', 'octetStringOrderingMatch')
+initOID('2.5.13.20', 'telephoneNumberMatch')
+initOID('2.5.13.21', 'telephoneNumberSubstringsMatch')
+initOID('2.5.13.23', 'uniqueMemberMatch')
+initOID('2.5.13.27', 'generalizedTimeMatch')
+initOID('2.5.13.28', 'generalizedTimeOrderingMatch')
+initOID('2.5.13.29', 'integerFirstComponentMatch')
+initOID('2.5.13.30', 'objectIdentifierFirstComponentMatch')
+initOID('2.5.13.31', 'directoryStringFirstComponentMatch')
+initOID('2.5.13.32', 'wordMatch')
+initOID('2.5.13.33', 'keywordMatch')
+
+initOID('2.5.18.1', 'createTimestamp')
+initOID('2.5.18.2', 'modifyTimestamp')
+initOID('2.5.18.3', 'creatorsName')
+initOID('2.5.18.4', 'modifiersName')
+initOID('2.5.18.9', 'hasSubordinates')
+initOID('2.5.18.10', 'subschemaSubentry')
+
+initOID('2.5.20.1', 'subschema')
+
+initOID('2.5.21.1', 'dITStructureRules')
+initOID('2.5.21.2', 'dITContentRules')
+initOID('2.5.21.4', 'matchingRules')
+initOID('2.5.21.5', 'attributeTypes')
+initOID('2.5.21.6', 'objectClasses')
+initOID('2.5.21.7', 'nameForms')
+initOID('2.5.21.8', 'matchingRuleUse')
+initOID('2.5.21.9', 'structuralObjectClass')
+initOID('2.5.21.10', 'governingStructureRule')
 
 // X.509 extension OIDs
 initOID('2.16.840.1.113730.1.1', 'nsCertType')
